f110f2a128bdb8ea411347fcc5c97b3c3c5cd534
amirmohamed <72021520+Amir94Mohamed@users.noreply.github.com>
2022-08-05 03:36:27 +0100

    Update Program.cs

--- FunctionalProgramming/Program.cs
+++ FunctionalProgramming/Program.cs
@@ -8,7 +8,7 @@ namespace FunctionalProgramming
         static void Main(string[] args)
         {
             //Cube(8);
-            Cost(127);
+            Cost(17);
             //fiveTimesTable(65);
             //Console.WriteLine(vector(1, 2));
             //UpperAndLowerBound(50,10);
